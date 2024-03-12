82c7833b662c74b3255a41a6b8defd50a9d2508a
Alexey <varlord12101994@gmail.com>
2024-03-12 10:52:39 +0200

    added_task02s

---
+++ task02s/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Введите целое полное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = n * (-1);
+
+while (count <= n)
+{
+    Console.Write(count + " ");
+    count++;
+}
