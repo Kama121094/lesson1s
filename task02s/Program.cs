0bd206341fe63807d285a05c4dc2af6c3687aa05
Alexey <varlord12101994@gmail.com>
2024-03-13 14:18:17 +0200

    added_task02snew

--- task02s/Program.cs
+++ task02s/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+// Напишите программу которая на входе принимает целое число N. А на выходке показывает все целе числа в промежутке от -N до N.
 
 Console.WriteLine("Введите целое полное число");
 int n = Convert.ToInt32(Console.ReadLine());
